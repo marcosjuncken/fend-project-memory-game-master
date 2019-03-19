b389f2fb9d0b0331f37144ec8604eadb078146f1
marcos.juncken <juncken.marcos@gmail.com>
2019-03-19 13:32:31 -0300

    Making the match game a success

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 /*
  * Create a list that holds all of your cards
  */
-
+var cards;
+cards = $('.card');
+cardsIcons = [
+    "fa fa-free-code-camp",
+    "fa fa-free-code-camp",
+    "fa fa-github",
+    "fa fa-github",
+    "fa fa-git",
+    "fa fa-git",
+    "fa fa-stack-overflow",
+    "fa fa-stack-overflow",
+    "fa fa-terminal",
+    "fa fa-terminal",
+    "fa fa-laptop",
+    "fa fa-laptop",
+    "fa fa-code",
+    "fa fa-code",
+    "fa fa-coffee",
+    "fa fa-coffee"
+];
 
 /*
  * Display the cards on the page
@@ -20,29 +39,102 @@ function shuffle(array) {
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
-    }
-
+    };
     return array;
-}
+};
 
-var cards;
-cards = $('.card');
+function replacingCards() {
+    counter = 0;
+    $('.stars').children().children()[0].className = 'fa fa-star';
+    $('.stars').children().children()[1].className = 'fa fa-star';
+    $('.stars').children().children()[2].className = 'fa fa-star';
+    $('.moves')[0].innerHTML = counter;
+    var newCards = shuffle(cardsIcons);
+    for (i = 0, len = newCards.length; i < len; i++) { 
+        $('.card').children()[i].className = newCards[i];
+        $('.card')[i].className = 'card';
+      };
+    return newCards
+};
+replacingCards();
 
 var openedCards = [];
 
+var counter = 0;
+
+var matchedCards = 0;
+
+function congrats() {
+    $('#congrats-modal').modal('show');
+    $('.modal-body')[0].innerHTML = 'Você realizou ' + counter + ' movimentos e ganhou ' + $('.fa-star').length + ' estrelas de 3.' 
+};
+
+function newMove() {
+    counter += 1;
+    $('.moves')[0].innerHTML = counter;
+    if (counter > 15) {
+        $('.stars').children().children()[2].className = 'fa fa-star-o';
+    };
+    if (counter > 20) {
+        $('.stars').children().children()[1].className = 'fa fa-star-o';
+    };
+    if (counter > 25) {
+        $('.stars').children().children()[0].className = 'fa fa-star-o';
+    };
+};
+
+function matchCards(card1, card2) {
+    card1.removeClass('open show').addClass('match animated bounce');
+    card2.removeClass('open show').addClass('match animated bounce');
+    openedCards = [];
+    matchedCards += 1;
+    if (matchedCards === 8) {
+        congrats();
+    }
+}
+
+function unmatchCards(card1, card2) {
+    card1.addClass('animated swing unmatch');
+    card2.addClass('animated swing unmatch');
+    setTimeout(function () {card1.removeClass('open show animated swing unmatch'); card2.removeClass('open show animated swing unmatch'); }, 900);
+    openedCards = [];
+}
+
 function flipCard(card) {
-   card.addClass('open show');
+    if (card.hasClass('match') === false) {
+        card.addClass('open show animated flipInX');
+        setTimeout(function () {card.removeClass('animated flipInX'); }, 700);
+    };
 };
 
 function addOpenCard(card) {
     openedCards.push(card);
+    if (openedCards.length === 2) {
+        newMove();
+        if (openedCards[0].children()[0].outerHTML === openedCards[1].children()[0].outerHTML) {
+            matchCards(openedCards[0],openedCards[1]);
+        } 
+        else {
+            unmatchCards(openedCards[0],openedCards[1]);
+        };
+    };
 };
 
 cards.each(function() {
     $(this).click(function() {
         flipCard($(this));
-        addOpenCard($(this));
+        setTimeout(addOpenCard, 900, $(this));
 })});
+
+$('.restart').click(function() {
+    replacingCards(cards);
+})
+
+$('#modal-restart').click(function() {
+    replacingCards(cards);
+    $('#exampleModal').modal('hide')
+})
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
